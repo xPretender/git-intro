e69641e4a3eabfcf6ed1ecf1e51acaeade0c79a2
Simon Urmatam <urmatamsimon@gmail.com>
2023-08-29 15:36:32 +0800

    React App

---
+++ github_activity/src/app/page.js
@@ -0,0 +1,15 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="Intro"></div>
+      <div className="Step1"></div>
+      <div className="Step2"></div>
+      <div className="Step3"></div>
+      <div className="Step4"></div>
+      <div className="Step5"></div>
+      <div className="Step6"></div>
+    </main>
+  );
+}

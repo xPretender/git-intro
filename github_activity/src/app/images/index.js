945a27004cb24d447b6ce32d83607f934b21caf2
Simon Urmatam <urmatamsimon@gmail.com>
2023-08-29 23:05:37 +0800

    React App Progress

---
+++ github_activity/src/app/images/index.js
@@ -0,0 +1,11 @@
+const IMAGES = {
+img1: require('./Picture1.png').default,
+img2: require('./Picture2.png').default,
+img3: require('./Picture3.png').default,
+img4: require('./Picture4.png').default,
+img5: require('./Picture5.png').default,
+img6: require('./Picture6.png').default,
+img7: require('./Picture7.png').default,
+img8: require('./Picture8.png').default,
+}
+export default IMAGES;

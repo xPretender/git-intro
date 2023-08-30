a4d4925e25672fd62a4b03dd32ae0b2d1617cb37
Simon Urmatam <urmatamsimon@gmail.com>
2023-08-30 20:19:58 +0800

    Added css styling and fixed README.md

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# "Introduction to Git and GitHub."
-## "I am a good person :P"
-### "Testing GitHub"
+# Introduction to Github and Git Activity
+
+This is a repository for the Introduction to Github and Git activity. 
+
+## Table of Contents
+- [Project Overview](#project-overview)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Usage](#usage)
+- [Configuration](#configuration)
+- [Contact](#contact)
+## Project Overview
+
+This repository is made to practice using Github and Git. The students tried to create a repository, clone it, and push changes to it.
+They also collaborated with each other to create a simple Next JS application with React.
+
+
+## Getting Started
+
+This is not made to be deployed. It is only for educational purposes.
+However, if you want to run it locally, follow the steps below.
+
+First, clone or download the ZIP of the repository.
+
+Then run the following commands in the root directory of the project.
+
+bash```
+npm install```
+
+### Prerequisites
+
+- Node.js
+- npm
+- dependencies in package.json
+
+
+### Usage
+
+ After successfully installing the dependencies, you may now view the simple Next JS application by running the following command in the root directory of the project.
+
+ bash ```
+    npm run dev```
+
+ Screenshots
+
+ ![Screenshot 1](/src/images/screenshots1.png)
+
+![Screenshot 2](/src/images/screenshots2.png)
+    
--- github_activity/src/app/page.js
+++ github_activity/src/app/page.js
@@ -1,86 +1,102 @@
-import styles from './page.module.css';
-import Image from 'next/image';
-import IMAGES from './images/index.js'
+import styles from "./page.module.css";
+import Image from "next/image";
+import IMAGES from "./images/index.js";
 
 export default function Home() {
   return (
     <main>
-    <div className="Intro">
-     <h1 className="title">Laboratory Activity: Introduction to Github</h1>
-     <p className="text">In this activity, we explored git and githubs functionalities and find out its usefulness in programming and project managing.
-       <br></br>We used VSCode as our IDE and its built-in terminal to execute git commands.<br></br> We used Github's website to create a repository and
-       to push our local repository to the cloud. <br></br>We also used Github's website to create a pull request and merge it to the main branch.
-     </p>
-    </div>
-    
-    <h1 className="title">Step 1: Downloading Git</h1>
-    <div className='Step1'>
-     <Image alt="screenshots" 
-     className="image"
-     src={IMAGES.img1}
-     />
-     <p className="text">Both of the students already had Git installed in their respective systems. We executed the "git --version" command to confirm its installation.
-       We also already had Github accounts prior to this activity.
-     </p>
-    </div>
-    <h1 className="title">Step 2: Git Basics</h1>
-     <div className='Step2'>
-       
-       <Image alt="screenshots" 
-        className="image"
-     src={IMAGES.img2}
-     />
-       <p className="text">We successfully created a directory, and initialize git on the directory. However, a problem occur at number 4 since "touch README.md" is a unix command and will not work on windows.
-         We simply created a README.md file manually. We also successfully added the README.md file to the staging area and committed it.
-       </p>
-     </div>
-     <h1 className="title">Step 3: Collaborative Development on GitHub</h1>
-     <div className='Step3'>
-       
-       <Image alt="screenshots" 
-        className="image"
-     src={IMAGES.img3}
-     />
-       <p className="text">We created a repository on Github with no issues. We also are able to push our local repository to the Github repository we made.</p>
-     </div>
-     <h1 className="title">Step 4: Working with Branches</h1>
-     <div className='Step4'>
-     
-     <Image alt="screenshots" 
-      className="image"
-     src={IMAGES.img4}
-     />
-       <p className="text">We are able to create a branch named feature-branch and push the new changes on the repository.</p>
-     </div>
-     <h1 className="title">Step 5: Pull Requests on GitHub</h1>
-     <div className='Step5'>
-     
-     <Image alt="screenshots" 
-      className="image"
-     src={IMAGES.img5}
-     />
-     <Image alt="screenshots" 
-      className="image"
-     src={IMAGES.img6}
-     />
-       <p className="text">After pushing the changes, we are tasked to merge main and feature-branch. This is done without any issues.</p>
-     </div>
-     <h1 className="title">Step 6: Clone and Collaborate</h1>
-     <div className='Step6'>
-     
-     <Image alt="screenshots" 
-      className="image"
-     src={IMAGES.img7}
-     />
-     <Image alt="screenshots" 
-      className="image"
-     src={IMAGES.img8}
-    />
-       <p className="text">We used the same repository in this activity for the collaborative part. First, Alzona added Ducusin as a collaborator for the repository.
-         Then, Ducusin cloned the repository to his local machine. He then created a new branch named react and pushed it to the repository. Lastly, Ducusin
-         created a pull request and Alzona merged it to the main branch.
-       </p>
-     </div>
-     </main>
+      <div className="Intro">
+        <h1 className="title">Laboratory Activity: Introduction to Github</h1>
+        <p className="text">
+          In this activity, we explored git and GitHub's functionalities and
+          found out its usefulness in programming and project managing.
+          <br />
+          We used VSCode as our IDE and its built-in terminal to execute git
+          commands.
+          <br />
+          We used GitHub's website to create a repository and to push our local
+          repository to the cloud. <br />
+          We also used GitHub's website to create a pull request and merge it
+          into the main branch.
+        </p>
+      </div>
+      <br></br>
+      <div className="card">
+        <Image alt="screenshots" className="card-image" src={IMAGES.img1} />
+        <div className="card-content">
+          <h1 className="card-title">Step 1: Downloading Git</h1>
+          <p className="card-text">
+            Both of the students already had Git installed in their respective
+            systems...
+          </p>
+        </div>
+      </div>
+
+      <div className="card">
+        <Image alt="screenshots" className="card-image" src={IMAGES.img2} />
+        <div className="card-content">
+          <h1 className="card-title">Step 2: Git Basics</h1>
+          <p className="card-text">
+            We successfully created a directory, and initialize git on the
+            directory. However, a problem occur at number 4 since "touch
+            README.md" is a unix command and will not work on windows. We simply
+            created a README.md file manually. We also successfully added the
+            README.md file to the staging area and committed it.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <Image alt="screenshots" className="card-image" src={IMAGES.img3} />
+        <div className="card-content">
+          <h1 className="card-title">
+            Step 3: Collaborative Development on GitHub
+          </h1>
+          <p className="card-text">
+            We created a repository on Github with no issues. We also are able
+            to push our local repository to the Github repository we made.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <Image alt="screenshots" className="card-image" src={IMAGES.img1} />
+        <div className="card-content">
+          <h1 className="card-title">Step 4: Working with Branches</h1>
+          <p className="card-text">
+            We are able to create a branch named feature-branch and push the new
+            changes on the repository.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image-container">
+          <Image alt="screenshots" className="image" src={IMAGES.img5} />
+          <Image alt="screenshots" className="image" src={IMAGES.img6} />
+        </div>
+        <div className="card-content">
+          <h1 className="card-title">Step 5: Pull Requests on GitHub</h1>
+          <p className="card-text">
+            After pushing the changes, we are tasked to merge main and
+            feature-branch. This is done without any issues.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <div className="image-container">
+          <Image alt="screenshots" className="image" src={IMAGES.img7} />
+          <Image alt="screenshots" className="image" src={IMAGES.img8} />
+        </div>
+        <div className="card-content">
+          <h1 className="card-title">Step 6: Clone and Collaborate</h1>
+          <p className="card-text">
+            We used the same repository in this activity for the collaborative
+            part. First, Alzona added Ducusin as a collaborator for the
+            repository. Then, Ducusin cloned the repository to his local
+            machine. He then created a new branch named react and pushed it to
+            the repository. Lastly, Ducusin created a pull request and Alzona
+            merged it to the main branch.
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
